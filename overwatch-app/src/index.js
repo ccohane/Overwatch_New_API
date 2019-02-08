7c3eb32c312f1d1b9da6c2dcf492e72d496bf862
ccohane <ccohane@gmail.com>
2019-02-08 16:10:58 -0500

    Created react app for my new cool app

---
+++ overwatch-app/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class navbar extends React.Component {
+    /**
+     * What goes in the navbar
+     * Schedule
+     * Players
+     * Teams
+     * Analysis of old 
+     */
+
+}
+
+class 
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
